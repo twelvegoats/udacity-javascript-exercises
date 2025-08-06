3afb1bbc7dd339439117158372a2faf79890000b
Sean Wildman <twelvegoats@protonmail.com>
2025-08-06 11:57:34 -0400

    Refactor destructuring exercise for clarity and consistency.
    I learned how to destructue an object in JavaScript, which is a powerful and efficient way to extract values from objects. The provided code demonstrates how destructuring can be used to simplify the process of accessing and manipulating object properties. By using destructuring, we can assign the properties of an object to variables in a more concise and readable way. This refactoring improves the code's readability and maintainability.

--- lesson-1-javascript-syntax/exercises/destructuring-template-literals/starter/js/scripts.js
+++ lesson-1-javascript-syntax/exercises/destructuring-template-literals/starter/js/scripts.js
@@ -1,33 +1,31 @@
 const user = {
-    firstName: "Whiskers",
-    lastName:"McOtter",
-    city:"Seattle",
-    state:"Washington",
-    favorites:["Frappuccinos", "fish"],
-    profilePic:"./images/otter_profile.webp"
-}
+  firstName: 'Whiskers',
+  lastName: 'McOtter',
+  city: 'Seattle',
+  state: 'Washington',
+  favorites: ['Frappuccinos', 'fish'],
+  profilePic: './images/otter_profile.webp',
+};
 
-const destructureExercise = async() => {
-    // This is where your destructuring should take place. destructure the user object provided and then use template literals to create the name and bio. If you destructure the user object completely you won't need to worry about the profilePic but it will show up on the page.
+const destructureExercise = async () => {
+  // This is where your destructuring should take place. destructure the user object provided and then use template literals to create the name and bio. If you destructure the user object completely you won't need to worry about the profilePic but it will show up on the page.
+  const { firstName, lastName, city, state, favorites, profilePic } = user;
+  // use template literals here with your destructured object
+  const name = `${firstName} ${lastName}`;
+  const bio = `Hi! My name is ${firstName} ${lastName} and I'm from ${city}, ${state}. Some of my favorite things are ${favorites[0]} and ${favorites[1]}.`;
 
-    // use template literals here with your destructured object
-    const name = 'EDIT THIS'
-    const bio  = 'EDIT THIS'
+  // DO NOT EDIT BELOW THIS LINE!
 
-    // DO NOT EDIT BELOW THIS LINE!
+  const card2 = document.querySelector('.userPop .userInfo');
+  const pic2 = document.querySelector('.userPop .picFrame');
+  const header = document.createElement('h2');
+  const biography = document.createElement('p');
+  const pic = document.createElement('img');
+  pic.src = `${profilePic}`;
+  header.textContent = name;
+  biography.textContent = bio;
+  pic2.append(pic);
+  card2.append(header, biography);
+};
 
-    const card2 = document.querySelector('.userPop .userInfo')
-    const pic2 = document.querySelector('.userPop .picFrame')
-    const header = document.createElement('h2')
-    const biography = document.createElement('p')
-    const pic = document.createElement('img')
-    pic.src = `${profilePic}`
-    header.textContent = name
-    biography.textContent = bio
-    pic2.append(pic)
-    card2.append(header, biography)
-
-}
-
-destructureExercise()
-
+destructureExercise();
