c8b9f0e8811aa090708daf667a87e793633640d6
Sean Wildman <twelvegoats@protonmail.com>
2025-08-13 18:13:43 -0400

    End result

---
+++ lesson-4-event-delegation/exercises/starter/js/scripts.js
@@ -0,0 +1,12 @@
+const handleClick = async (event) => {
+  const productCard = event.target.closest('.product-card');
+  const selectedCard = productCard.querySelector('.product-name').textContent;
+  alert(`You selected the ${selectedCard}. Good Choice!`);
+};
+
+const cardListener = async () => {
+  const cardContainer = document.querySelector('.product-grid');
+  cardContainer.addEventListener('pointerdown', handleClick);
+};
+
+cardListener();
