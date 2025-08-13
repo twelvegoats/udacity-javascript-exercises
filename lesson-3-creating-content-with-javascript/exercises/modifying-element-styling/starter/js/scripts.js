7b537ae1515a0d6d2b3fff311c1b5e0390911f1a
Sean Wildman <twelvegoats@protonmail.com>
2025-08-13 16:28:08 -0400

    Style update

--- lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
+++ lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
@@ -1,6 +1,12 @@
-const restyleOtter = async() => {
-    // Write your code here
-}
+const restyleOtter = async () => {
+  const profileCard = document.querySelector('.oldProfileCard');
+  const body = document.querySelector('body');
+  const h1 = document.querySelector('h1');
+
+  profileCard.classList.remove('oldProfileCard');
+  profileCard.classList.add('profileCard');
+  body.style.backgroundColor = '#97382A';
+  h1.style.color = 'white';
+};
 
 restyleOtter();
-
