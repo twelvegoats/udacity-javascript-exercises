c83d81e29864aac3fc06f2cf2b4ef25dd6d3a1e4
Sean Wildman <twelvegoats@protonmail.com>
2025-08-13 16:30:45 -0400

    Add title class

--- lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
+++ lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
@@ -1,12 +1,6 @@
 const restyleOtter = async () => {
-  const profileCard = document.querySelector('.oldProfileCard');
-  const body = document.querySelector('body');
-  const h1 = document.querySelector('h1');
-
-  profileCard.classList.remove('oldProfileCard');
-  profileCard.classList.add('profileCard');
-  body.style.backgroundColor = '#97382A';
-  h1.style.color = 'white';
+  const titleDiv = document.querySelector('body').firstElementChild;
+  titleDiv.classList.add('title');
 };
 
 restyleOtter();
