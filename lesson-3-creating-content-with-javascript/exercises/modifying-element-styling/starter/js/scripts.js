72484bbd1accc5bdd91adca185ca48a7cb141d6d
Sean Wildman <twelvegoats@protonmail.com>
2025-08-13 16:47:54 -0400

    Add id to cardContainer

--- lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
+++ lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
@@ -1,6 +1,18 @@
 const restyleOtter = async () => {
   const title = document.querySelector('body').firstElementChild;
   title.classList.add('title');
+
+  const cardContainer = document.querySelector('.title').nextElementSibling;
+  cardContainer.id = 'card-container';
+
+  const profileCard = cardContainer.firstElementChild;
+  profileCard.classList.remove('oldProfileCard');
+  profileCard.classList.add('profileCard');
+  profileCard.style.removeProperty('background-color');
+  profileCard.style.removeProperty('color');
+
+  const picFrame = profileCard.firstElementChild;
+  picFrame.classList.add('picFrame');
 };
 
 restyleOtter();
